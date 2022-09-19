369a6751ae4003f29980b20e5c6504730a215146
bbakercello <bbakercello@gmail.com>
2022-09-19 14:27:48 -0400

    styled post elelements

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,7 +4,9 @@ import ReactPlayer from 'react-player/'
 const Post = (props) => {
   return (
     <div className="post-container">
+        <h4 className="username"><img class="profile-pic" src="https://picsum.photos/200/300" alt="profile-pic"/>Username</h4>
         <div className='player-wrapper'>
+        
         <ReactPlayer
           className='react-player'
           url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
