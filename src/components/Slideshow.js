49d0debef2d928264f5de426450959e7902d750f
hspinks2692 <hspinks2692@yahoo.com>
2022-09-19 15:23:11 -0400

    slideshow created

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Slide } from 'react-slideshow-image'
+import 'react-slideshow-image/dist/styles.css'
+
+const slideImages = [
+  {
+    url: 'https://i.imgur.com/IXUYGjF.png',
+    caption: 'Slide 1'
+  },
+  {
+    url: 'https://i.imgur.com/su7HYwJ.png',
+    caption: 'Slide 2'
+  },
+  {
+    url: 'https://i.imgur.com/6a6xTLY.png',
+    caption: 'Slide 3'
+  },
+];
 
 const Slideshow = () => {
-  return (
-    <div className="slideshow-container">Slideshow</div>
-  )
+    return (
+      <div className="slide-container">
+        <Slide>
+         {slideImages.map((slideImage, index)=> (
+            <div className="each-slide" key={index}>
+              <div style={{'backgroundImage': `url(${slideImage.url})`}}>
+                <span>{slideImage.caption}</span>
+              </div>
+            </div>
+          ))} 
+        </Slide>
+      </div>
+    )
 }
 
 export default Slideshow
