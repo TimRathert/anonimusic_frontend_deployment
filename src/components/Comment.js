6e0868d9719005481051b28269fb2474b323ed87
bbakercello <bbakercello@gmail.com>
2022-09-19 17:07:36 -0400

    styled comment boxes

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -5,12 +5,14 @@ import Collapsible from 'react-collapsible'
 const Comment = () => {
  
 return (
-    <>
-    <Collapsible trigger="Start here">
-    <p>comment</p>
+    <div className="comment">
+    <Collapsible className="comment-title" trigger="Comments">
+    <p className="collapsible">this sucks man</p>
+    <p className="collapsible">yeah seriously</p>
+    <p className="collapsible">based</p>
 
     </Collapsible>
-    </>
+    </div>
 )}
 
 export default Comment
