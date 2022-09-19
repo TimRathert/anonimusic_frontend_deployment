08945fccc60de0f60c8209b9b5b99be600a80e04
bbakercello <bbakercello@gmail.com>
2022-09-19 13:57:41 -0400

    react player working and additional homepage styling

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,6 +4,14 @@ import ReactPlayer from 'react-player/'
 const Post = (props) => {
   return (
     <div className="post-container">
+        <div className='player-wrapper'>
+        <ReactPlayer
+          className='react-player'
+          url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+          width='100%'
+          height='100%'
+        />
+      </div>
         <p>Description</p>
         <div>{props.Comment}</div>
     </div>
