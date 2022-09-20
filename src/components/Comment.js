28be64e03ee4e714a86f7a3a9c2418ca03907484
bbakercello <bbakercello@gmail.com>
2022-09-20 15:23:54 -0400

    get comments use state function written

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {useState} from 'react'
 import Collapsible from 'react-collapsible'
 
 const Comment = () => {
- 
+
+const [comments, setComments] = useState([]);
+
+const getComments = async () => {
+    // const response = await fetch(url);
+    const data = await response.json;
+    setComments(data.results)
+}
+
+useEffect(()=> {
+    getComments();
+}, []);
+
+const loaded = () => {
 return (
     <div className="comment">
     <Collapsible className="comment-title" trigger="Comments">
@@ -14,4 +27,10 @@ return (
     </div>
 )}
 
+const loading = () => {
+    return(<h1>Loading...</h1>)
+}
+
+return comments ? loaded() : loading();
+}
 export default Comment
